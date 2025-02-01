3e33b7549a644a4cafc5f7c8c2631f51e7f1ee64
Gelaz el chingon <galfan18kasec@gmail.com>
2025-02-01 13:22:28 -0600

    moves router to app, replaced nextui to heroui, added some ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@heroui/react";
+// Import the generated route tree
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+const router = createRouter({ routeTree })
+
 import './App.css'
+import { useState } from "react";
+
+const API_URL = 'http://192.168.1.10:3000'
+const REMOVE_ALL_USERS_URL = `${API_URL}/clean`
+
 
 function App() {
+  const [resetButton, setResetButton] = useState(false);
+  const cancelAction = () => {
+    setResetButton(false)
+  }
+  const triggerResetAccounts = () => {
+    setResetButton(true);
+  }
+  const resetAccounts = () => {
+    console.log('REseetiting accounts')
+    fetch(REMOVE_ALL_USERS_URL).then(res => console.info("everything got well", { res })).catch(err => console.error('Something got wrong', { err }))
+  }
+
   return (
     <>
-      <Navbar position="static">
+      <Navbar>
         <NavbarBrand>
           <p className="font-bold text-inherit">Billar Client Network</p>
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-4" justify="center">
           <NavbarItem>
-            <Link color="foreground" href="#">
-              Connected Users
+            <Link color="foreground" href="/radacct-logs">
+              Radius Accounts
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="/radcheck-logs">
+              Radius Check
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="/radpostauth-logs">
+              Radius Post Auth
             </Link>
           </NavbarItem>
           <NavbarItem isActive>
-            <Link color="foreground" href="#">
+            <Link color="foreground" href="/create-new-user">
               Create new user
             </Link>
           </NavbarItem>
         </NavbarContent>
         <NavbarContent justify="end">
           <NavbarItem>
-            <Button as={Link} color="primary" href="#" variant="flat">
-              Reset database
-            </Button>
+            {resetButton ?
+              <>
+                <Button color="default" href="#" variant="flat" onClick={cancelAction}>
+                  Cancel Action
+                </Button>
+                <Button color="danger" href="#" variant="flat" onClick={resetAccounts}>
+                  Reset Accont
+                </Button>
+              </>
+              : <>
+                <Button color="primary" href="#" variant="flat" onClick={triggerResetAccounts}>
+                  Try to RESET Acconts
+                </Button>
+              </>}
           </NavbarItem>
         </NavbarContent>
       </Navbar>
       <section>
-
+        <RouterProvider router={router} />
       </section>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { NextUIProvider } from '@nextui-org/react'
+import { HeroUIProvider } from '@heroui/react'
 import './index.css'
 
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import App from './App'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
 
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NextUIProvider>
-      <RouterProvider router={router} />
-    </NextUIProvider>
+    <HeroUIProvider>
+      <App />
+    </HeroUIProvider>
   </StrictMode>,
 )
